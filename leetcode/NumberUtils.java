dfd75d74e08d6372fd4c6c6eed2b329c0a6e9778
ChenLaiHong <1185630400@qq.com>
2018-08-31 20:04:35 +0800

    回文数

--- leetcode/NumberUtils.java
+++ leetcode/NumberUtils.java
@@ -29,4 +29,33 @@ public class NumberUtils {
      * */
 
 
+/*
+* 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+* */
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }else{
+            if(x != 0 && x % 10 == 0){
+                return false;
+            }
+            int tamp = 0;
+            while(x>tamp){
+                tamp = tamp * 10 + x % 10;
+                x /= 10;
+            }
+            return tamp==x || x == tamp/10;
+        }
+    }
+
+    /*
+    *输入: 10
+     输出: false
+     解释: 从右向左读, 为 01 。因此它不是一个回文数。
+     输入: -121
+     输出: false
+     解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+     输入: 121
+     输出: true
+    * */
 }
